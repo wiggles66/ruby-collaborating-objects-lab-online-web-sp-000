5ed0afce017d6127596335e529e26de4456642f1
Michael Waide <mvwaide@gmail.com>
2020-07-20 18:47:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,21 @@
-
 class Song
+  attr_accessor :name, :artist
 
-attr_accessor :name, :artist
+  def initialize(name)
+    @name = name
+  end
 
-def initialize(name)
-  @name = name
-end
+  def self.new_by_filename(file)
+    artist_name = file.split(" - ")[0]
+    song_name = file.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist_name
+    song
+  end
 
-def self.new_by_filename(file_name)
-  song = file_name.split(" - ")[1]
-  artist = file_name.split(" - ")[0]
-  new_song = self.new(song)
-  new_song.artist_name = artist
-  new_song
-end
-
-def artist_name=(name)
-  self.artist = Artist.find_or_create_by_name(name)
-  artist.add_song(self)
-end
-
-end
+  def artist_name=(name)
+    artist = Artist.find_or_create_by_name(name)
+    self.artist = artist
+    artist.add_song(self)
+  end
+end 
