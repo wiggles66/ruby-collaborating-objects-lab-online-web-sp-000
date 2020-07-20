ad7831602a4b2c7526ca6aeb4a7e6ac92fb33654
Michael Waide <mvwaide@gmail.com>
2020-07-20 18:47:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,18 +1,15 @@
-
 class MP3Importer
+  attr_accessor :path
 
-attr_accessor :path
+  def initialize(path)
+    @path = path
+  end
 
-def initialize(path)
-  @path = path
-end
+  def files
+    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
+  end
 
-def files
-  @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
-end
-
-def import
-  files.each {|song| Song.new_by_filename(song)}
-end
-
-end
+  def import
+    files.each { |file| Song.new_by_filename(file) }
+  end
+end 
